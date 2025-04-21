0f8fc6a5df286144507b218924238c2c9ac638e6
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-04-21 19:48:44 +0530

    db config

--- url-shortener/src/main/java/com/url/shortener/entity/UrlMapping.java
+++ url-shortener/src/main/java/com/url/shortener/entity/UrlMapping.java
@@ -1,4 +1,29 @@
 package com.url.shortener.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
 public class UrlMapping {
+
+    // many to one relationship with user
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount=0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
 }
