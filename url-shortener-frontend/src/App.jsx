373cc036eb574138b6e77cefdf9c92d447aeaa2b
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-04-24 20:50:20 +0530

    Completed Login functionality

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,25 +1,32 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import NavBar from './components/Navbar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast';
+
+import LoginPage from './components/LoginPage'
 
 function App() {
  
 
   return (
     <>
-      <BrowserRouter>
-      <NavBar/>
+      <Router>
+      <NavBar />
+      <Toaster position='bottom-center'/>
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
 
         </Routes>
         <Footer/>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
